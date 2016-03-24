731521b10508d0523e5faaee94e8d776abda66bb
Matt Venables <mattvenables@gmail.com>
2016-03-23 22:51:24 -0400

    Add remaining vimrc config, colors

--- node/install.sh
+++ node/install.sh
@@ -1,19 +1,23 @@
-if test ! $(which spoof)
+if test ! $(which grunt)
 then
-  npm install spoof -g
+  echo "Installing grunt-cli ..."
+  npm install grunt-cli -g
 fi
 
 if test ! $(which nodemon)
 then
+  echo "Installing nodemon ..."
   npm install nodemon -g
 fi
 
 if test ! $(which jscs)
 then
+  echo "Installing jscs ..."
   npm install jscs -g
 fi
 
 if test ! $(which jshint)
 then
+  echo "Installing jshint ..."
   npm install jshint -g
 fi	
