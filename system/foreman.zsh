1505e6bde5f502930f2771efb8d26c58c879813d
Matt Venables <mattvenables@gmail.com>
2016-08-14 20:12:27 -0400

    Prefer node-foreman

--- system/foreman.zsh
+++ system/foreman.zsh
@@ -1,2 +1,2 @@
 # If a Procfile.local file exists, use it. Otherwise run foreman normally
-alias f='[ -e Procfile.local ] && foreman start -f Procfile.local || foreman start'
+alias f='[ -e Procfile.local ] && nf start --procfile Procfile.local || nf start'
